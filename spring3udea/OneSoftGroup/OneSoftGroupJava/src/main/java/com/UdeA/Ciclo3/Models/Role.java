b85281686224ff0ee652e90e95a4ca5dca7a7e49
AGUIAR-SEBAS <87918288+AGUIAR-SEBAS@users.noreply.github.com>
2023-10-16 22:24:21 -0500

    Delete spring3udea/OneSoftGroup/OneSoftGroupJava directory

--- spring3udea/OneSoftGroup/OneSoftGroupJava/src/main/java/com/UdeA/Ciclo3/Models/Role.java
+++
@@ -1,9 +0,0 @@
-package com.UdeA.Ciclo3.Models;
-
-import javax.persistence.*;
-import java.util.List;
-public enum Role {
-
-    ADMINISTRADOR,OPERATIVO;
-
-}
--- spring3udea/OneSoftGroup/OneSoftGroupJava/src/main/java/com/UdeA/Ciclo3/Repository/EmpresaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.UdeA.Ciclo3.Repository;
-
-import com.UdeA.Ciclo3.Models.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
-}
